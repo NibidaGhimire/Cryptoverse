0d7750055c43439b4e31b7b9a8a6247db21db869
NibidaGhimire <nibidaghimire39@gmail.com>
2024-01-18 11:40:22 +0545

    first commit

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,18 @@
+
+// const options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/coins',
+//     params: {
+//       referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//       timePeriod: '24h',
+//       'tiers[0]': '1',
+//       orderBy: 'marketCap',
+//       orderDirection: 'desc',
+//       limit: '50',
+//       offset: '0'
+//     },
+//     headers: {
+//       'X-RapidAPI-Key': 'c3ea7b21d1msh69c2fe2f23357aap19c22fjsn4ca21bacc35f',
+//       'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+//     }
+//   };
